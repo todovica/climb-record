96b06a13467bd2390d8c9c2359e49d4277c94c69
todovica <ana.todovic@gmail.com>
2019-11-10 00:52:57 +0100

    Updates

---
+++ precache-manifest.3f5d4846052406acdbca11eb67e01d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a88ee0c350ebe8d6dae51f388cef93e",
+    "url": "/climb-record/index.html"
+  },
+  {
+    "revision": "9cf1296a9766adb6d6aa",
+    "url": "/climb-record/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "09d1029a9d036c62c3fe",
+    "url": "/climb-record/static/js/2.027993aa.chunk.js"
+  },
+  {
+    "revision": "9cf1296a9766adb6d6aa",
+    "url": "/climb-record/static/js/main.a4c8fd86.chunk.js"
+  },
+  {
+    "revision": "496fe92a19717534103d",
+    "url": "/climb-record/static/js/runtime-main.98279a18.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/climb-record/static/media/logo.25bf045c.svg"
+  }
+]);
