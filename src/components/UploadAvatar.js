2c6666bd3d9aa2fd55fa975f35780dfba563f8e7
todovica <ana.todovic@gmail.com>
2019-11-14 14:26:02 +0100

    move dialog components to be reused in other places fix a little bit padding for welcome component

--- src/components/UploadAvatar.js
+++ src/components/UploadAvatar.js
@@ -4,20 +4,12 @@ import { updateUser } from "../actions/index";
 import { userService } from '../services';
 import Avatar from 'react-avatar-edit'
 
-import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import MuiDialogContent from '@material-ui/core/DialogContent';
-import MuiDialogActions from '@material-ui/core/DialogActions';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import Typography from '@material-ui/core/Typography';
+import { DialogTitle, DialogContent, DialogActions } from './DialogComponents';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
-
-
 
 
 function mapStateToProps(state) {
@@ -31,47 +23,6 @@ function mapStateToProps(state) {
     }
   };
 }
-
-
-const styles = theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-});
-
-const DialogTitle = withStyles(styles)(props => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
-
-const DialogContent = withStyles(theme => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiDialogContent);
-
-const DialogActions = withStyles(theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
 
 function UploadAvatar(props) {
 
@@ -121,7 +72,7 @@ function UploadAvatar(props) {
   return (
     <div>
       <Button color="secondary" onClick={handleClickOpen} >
-        {(user.image) ? <img src={user.image} alt="Profie Photo" /> : <AccountCircleIcon style={{ width: "150px", height: "150px" }} />}
+        {(user.image) ? <img src={user.image} alt="Profie" style={{ width: "150px", height: "150px" }} /> : <AccountCircleIcon style={{ width: "150px", height: "150px" }} />}
       </Button>
       <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
         <DialogTitle id="customized-dialog-title" onClose={handleClose}>
--- src/components/WelcomeCardComponent.js
+++ src/components/WelcomeCardComponent.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { FaSpa } from "react-icons/fa";
 import UploadAvatar from './UploadAvatar';
 import Grid from '@material-ui/core/Grid';
+import { Container } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 
 class WelcomeCardComponent extends React.Component {
 
     render() {
         return (
             <Grid item sx={12} sm={12} md={3}>
+                <Container style={{ marginTop: '10px' }}>
                 <UploadAvatar />
-                  <h5 className="card-title">Hi {this.props.user.firstName}! <FaSpa /></h5>
+                <h5 className="card-title">Hi {this.props.user.firstName}! <FaSpa /></h5>
                   <h6 className="card-subtitle mb-2 text-muted">Welcome to Climb Record App!!</h6>
                   <p className="card-text">This is a perfect place for you to keep track of climbing scores and get in touch with awesome people.</p>
+                </Container>
                 </Grid>
             );
         }
